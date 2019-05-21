ee4b61c94b50ff55f9ff87a384d51a009f72bcd1
Diwakar <dabbara.divakar@gmail.com>
2019-05-21 10:43:02 +0530

    Updates

---
+++ precache-manifest.2ac0058166427d997c7d9086a3b302e2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eb550282be57119a44141e77541ee09",
+    "url": "/axolot/index.html"
+  },
+  {
+    "revision": "3275167603f8030c67c0",
+    "url": "/axolot/static/css/2.c2fcad91.chunk.css"
+  },
+  {
+    "revision": "f47e80f252376ec7a061",
+    "url": "/axolot/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "3275167603f8030c67c0",
+    "url": "/axolot/static/js/2.9b65bf67.chunk.js"
+  },
+  {
+    "revision": "f47e80f252376ec7a061",
+    "url": "/axolot/static/js/main.085d572e.chunk.js"
+  },
+  {
+    "revision": "2a52a53aafe91c6ddb20",
+    "url": "/axolot/static/js/runtime~main.0a7ef571.js"
+  },
+  {
+    "revision": "9839afc0114844a605ac4b7dc2911c2c",
+    "url": "/axolot/static/media/1.9839afc0.png"
+  },
+  {
+    "revision": "87059425927c54ab220507838ab7ed5b",
+    "url": "/axolot/static/media/2.87059425.png"
+  },
+  {
+    "revision": "70466ca19be65bdda6bd88d8c10d72c6",
+    "url": "/axolot/static/media/3.70466ca1.png"
+  },
+  {
+    "revision": "c3e1d3b1fd996784c782f55ca3b58119",
+    "url": "/axolot/static/media/4.c3e1d3b1.png"
+  },
+  {
+    "revision": "65170490bed724b466f977ee99a81fee",
+    "url": "/axolot/static/media/about.65170490.png"
+  },
+  {
+    "revision": "ac32f98c566e5b7cc66d39a91cb46818",
+    "url": "/axolot/static/media/main-banner.ac32f98c.png"
+  }
+]);
